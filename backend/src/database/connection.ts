9bf32932b1459b40fd97aaee219a88a991cc7164
Nimira43 <n.rabone72@aol.com>
2024-12-31 23:21:33 +0000

    Soft reboot - project now called Echoverse. created package.json and frontend and backend folders.

--- backend/src/database/connection.ts
+++
@@ -1,9 +0,0 @@
-import { Sequelize } from 'sequelize'
-
-const connection = new Sequelize({
-  dialect: 'mysql',
-  host: 'localhost',
-  username: 'root',
-  password: 'root',
-  database: 'blog'
-})
