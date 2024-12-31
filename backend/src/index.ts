96ec0229917bdffed346cfa8abd95d31898fb0c1
Nimira43 <n.rabone72@aol.com>
2024-12-31 23:56:35 +0000

    Sending HTML

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -4,9 +4,14 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('Echoverse')
+  res.send(`
+    <div>
+      <h1>Echoverse</h1>
+    </div>  
+  `)
 })
 
 app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port} `)
+  console.log(`
+    Server is running on http://localhost:${port} `)
 })
